b46378b6e2291952931edc12e5f14a78b42c3254
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-01-06 10:30:50 +0300

    postgres, db, prisma added

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { artistsData } from "./songsData";
+
+const prisma = new PrismaClient();
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: "user@test.com" },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcrypt.hashSync("password", salt),
+    },
+  });
+};
+
+run()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
